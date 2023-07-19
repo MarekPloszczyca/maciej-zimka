b830e62b1ce31fdee100034b2d76e5977f1dcf57
Marobuc <marploszczyca@gmail.com>
2023-07-19 19:13:02 +0200

    major changes

--- Scripts/navigation.js
+++ Scripts/navigation.js
@@ -1,11 +1,23 @@
 export {menuButton, toggleMenuHandler};
 const menuButton = document.querySelector(".menu-icon");
 const burgerMenu = document.querySelector(".hidden");
+const burgerDropdown = document.querySelector(".burger-dropdown");
+const icon = burgerDropdown.querySelector("ion-icon");
+const bands = burgerDropdown.querySelector("ul");
 const content = document.querySelector(".container");
+
 
 const toggleMenuHandler = () => {
     menuButton.classList.toggle("change");
     content.classList.toggle("hidden");
     burgerMenu.classList.toggle("hidden");
   };
+
+
+  const dropdownHandler = () => {
+    icon.classList.toggle("transformed");
+    bands.classList.toggle("hidden");
+  }
+
+  burgerDropdown.addEventListener("click", dropdownHandler);
   menuButton.addEventListener("click", toggleMenuHandler);
--- Scripts/recenzje.js
+++ Scripts/recenzje.js
@@ -1,5 +1,14 @@
 import { menuButton, toggleMenuHandler } from "./navigation.js";
 
+const developingArrows = document.querySelectorAll(".down-arrow");
 
+function developReviewHandler(event) {
+  const review = this.closest(".review");
+  event.target.classList.toggle("transformed");
+  review.children[2].classList.toggle("hidden");
+}
 
+developingArrows.forEach((arrow) => {
+  arrow.addEventListener("click", developReviewHandler);
+});
 menuButton.addEventListener("click", toggleMenuHandler);
